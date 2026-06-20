7163db8a1cc2a84e6695e2a7a7bc53ab2c4ada31
sshen070 <SujithShen@gmail.com>
2026-06-19 23:04:32 -0700

    Enhanced pipeline to log traffic data at five-minute intervals

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,32 @@
 from data_extraction import return_url
+from database import *
 from trip_route import TripRoute
 
-# url = "https://api.tomtom.com/routing/1/calculateRoute/52.50931,13.42936:52.50274,13.43872/json?vehicleHeading=90&sectionType=traffic&report=effectiveSettings&routeType=eco&traffic=true&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=120&vehicleCommercial=false&vehicleEngineType=combustion&key=2cljWfCfDZqNG3egY6vWQzLOWc44S1pr"
-# print(requests.get(url))
 
 def main():
-    api_key = "2cljWfCfDZqNG3egY6vWQzLOWc44S1pr"
     
     # UCI --> UCR
     x1, y1, x2, y2 = ("33.643250", "-117.838606", "33.969535", "-117.332683")
     print(f'Coords #1: {x1}, {y1}\nCoords #2: {x2}, {y2}\n')
 
-    route_data = return_url(x1, y1, x2, y2, api_key)
-    # print(route_data)
-    
-    route = TripRoute(
-        lengthInMeters=route_data["lengthInMeters"],
-        travelTimeInSeconds=route_data["travelTimeInSeconds"],
-        trafficDelayInSeconds=route_data["trafficDelayInSeconds"],
-        trafficLengthInMeters=route_data["trafficLengthInMeters"],
-        departureTime=route_data["departureTime"],
-        arrivalTime=route_data["arrivalTime"]
-    )
+    while True:
+        
+        # Run query (fetch updated values)
+        route_data: dict = return_url(x1, y1, x2, y2, os.getenv("TOMTOM_API_KEY"))
+        
+        route: TripRoute = TripRoute(
+            lengthInMeters=route_data["lengthInMeters"],
+            travelTimeInSeconds=route_data["travelTimeInSeconds"],
+            trafficDelayInSeconds=route_data["trafficDelayInSeconds"],
+            trafficLengthInMeters=route_data["trafficLengthInMeters"],
+            departureTime=route_data["departureTime"],
+            arrivalTime=route_data["arrivalTime"]
+        )
+        
+        print(route)
 
-    print(route)
+        # Add route stats to database table
+        run_extraction(route)
 
 if __name__ == "__main__":
     main()
