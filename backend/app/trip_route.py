c0cd74fd2e2cba8c7f0321085460fcfde34e0a3d
sshen070 <SujithShen@gmail.com>
2026-06-19 22:59:04 -0700

    Renamed member variables

--- backend/app/trip_route.py
+++ backend/app/trip_route.py
@@ -14,18 +14,18 @@ class TripRoute:
 
     # Derived values
     distance_miles: float = field(init=False)
-    traffic_delay_miles: float = field(init=False)    
+    traffic_length_miles: float = field(init=False)    
 
     travel_time_min: float = field(init=False)
     traffic_delay_min: float = field(init=False)
 
-    d_date: str = field(init=False)
-    d_time: str = field(init=False)
-    d_time_zone: str = field(init=False)
+    departure_date: str = field(init=False)
+    departure_time: str = field(init=False)
+    departure_time_zone: str = field(init=False)
 
-    a_date: str = field(init=False)
-    a_time: str = field(init=False)
-    a_time_zone: str = field(init=False)
+    arrival_date: str = field(init=False)
+    arrival_time: str = field(init=False)
+    arrival_time_zone: str = field(init=False)
 
     def __post_init__(self):
 
@@ -33,20 +33,20 @@ class TripRoute:
         meters_to_miles: float = 1609.34
         min_to_sec: int = 60
 
-        self.distance_miles = self.lengthInMeters / meters_to_miles
-        self.traffic_delay_miles = self.trafficLengthInMeters / meters_to_miles
+        self.distance_miles = round(self.lengthInMeters / meters_to_miles, 2)
+        self.traffic_length_miles = round(self.trafficLengthInMeters / meters_to_miles, 2)
 
-        self.travel_time_min = self.travelTimeInSeconds / min_to_sec
-        self.traffic_delay_min = self.trafficDelayInSeconds / min_to_sec
+        self.travel_time_min = round(self.travelTimeInSeconds / min_to_sec, 2)
+        self.traffic_delay_min = round(self.trafficDelayInSeconds / min_to_sec, 2)
 
         # Parse timestamps
         dep = datetime.fromisoformat(self.departureTime)
         arr = datetime.fromisoformat(self.arrivalTime)
 
-        self.d_date = dep.strftime("%Y-%m-%d")
-        self.d_time = dep.strftime("%H:%M:%S")
-        self.d_time_zone = str(dep.tzinfo)
+        self.departure_date = dep.strftime("%Y-%m-%d")
+        self.departure_time = dep.strftime("%H:%M:%S")
+        self.departure_time_zone = str(dep.tzinfo)
 
-        self.a_date = arr.strftime("%Y-%m-%d")
-        self.a_time = arr.strftime("%H:%M:%S")
-        self.a_time_zone = str(arr.tzinfo)
+        self.arrival_date = arr.strftime("%Y-%m-%d")
+        self.arrival_time = arr.strftime("%H:%M:%S")
+        self.arrival_time_zone = str(arr.tzinfo)
